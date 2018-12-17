62cc0280308f078d513ac972fc5de68866e16de7
fiahfy <d.fiahfy@gmail.com>
2018-12-17 23:04:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# @fiahfy/electron-context-menu
+
+> Context menu helper for electron.
+
+## Installation
+```
+npm install @fiahfy/electron-context-menu
+```
+
+## Usage
+```js
+import show from '@fiahfy/electron-context-menu'
+
+window.addEventListener('contextmenu', () => {
+  show([
+    {
+      label: 'Ping',
+      click: () => console.log('pong')
+    }
+  ])
+})
+```
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { remote } from 'electron'
+
+const { Menu } = remote
+
+const appendInspectElementMenu = (template) => {
+  const { clientX: x, clientY: y } = window.event
+
+  if (template.length) {
+    template = template.concat([{ type: 'separator' }])
+  }
+
+  return template.concat([
+    {
+      label: 'Inspect Element',
+      click: () => remote.getCurrentWindow().inspectElement(x, y)
+    }
+  ])
+}
+
+export default (template = []) => {
+  if (process.env.NODE_ENV !== 'production') {
+    template = appendInspectElementMenu(template)
+  }
+
+  if (!template.length) {
+    return
+  }
+
+  Menu.buildFromTemplate(template).popup(remote.getCurrentWindow(), {
+    async: true
+  })
+}
