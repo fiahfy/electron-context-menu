f8225065b1d60553ddae57d42468ded0ea4306d4
fiahfy <d.fiahfy@gmail.com>
2025-08-27 21:58:44 +0900

    Upgrade packages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {
   BrowserWindow,
-  type IpcMainInvokeEvent,
+  type IpcMainEvent,
   Menu,
   type MenuItemConstructorOptions,
   ipcMain,
@@ -22,7 +22,7 @@ export type ContextMenuParams = {
 
 export type ActionCreators = {
   [type in string]: (
-    event: IpcMainInvokeEvent,
+    event: IpcMainEvent,
     params: ContextMenuParams,
     data?: ContextMenuOption['data'],
   ) => MenuItemConstructorOptions | false | undefined
@@ -71,7 +71,7 @@ export const register = (
 ) => {
   ipcMain.on(
     `${prefix}showContextMenu`,
-    (event: IpcMainInvokeEvent, params: ContextMenuParams) => {
+    (event: IpcMainEvent, params: ContextMenuParams) => {
       const findActionCreator = (type: string) => {
         return { ...actionCreators, ...defaultActionCreators }[type]
       }
